0455a02b3ddec2500eb6c2816bff61df0e925c33
Hergesheimer <berna_her@hotmail.com>
2022-11-28 01:10:04 -0300

    antes de dormir

--- index.js
+++ index.js
@@ -13,24 +13,30 @@ let mensaje = `Hola ${nombre}, elegí que productos queres cargar al carrito`;
 alert(mensaje);
 
 class Producto{
-    constructor(nombre, talle, precio){
+    constructor(nombre, talle, precio, cantidad){
         this.nombre = nombre;
         this.talle = talle;
         this.precio = precio;
-        this.vendido = false;
+        this.cantidad = cantidad;
     }
     vender(){
-        this.vendido = true;
-    }
-}
+        this.cantidad -= 1;   
+            }
+    if (cantidad = 0){
+     alert("Producto agotado")
+            } 
+     }
 
-const producto1 = new Producto ("Remera Nike azul manga corta", "M", 8050);
-const producto2 = new Producto ("Remera Adidas negra manga larga", "S", 10050);
-const producto3 = new Producto ("antalon Nike gris con bolsillos", "M", 16050);
-const producto4 = new Producto ("Pantalon Adidas rojo sin bolsillos", "L", 18050);
-const producto5 = new Producto ("Medias Nike blancas cortas", "M", 2050);
-const producto6 = new Producto ("Musculosa Under Armour amarilla", "S", 9050);
-const producto7 = new Producto ("Buzo Ascis verde poliester", "L", 12000);
+const producto1 = new Producto ("Remera Nike azul manga corta", "M", 8050, 3);
+const producto2 = new Producto ("Remera Adidas negra manga larga", "S", 10050, 5);
+const producto3 = new Producto ("antalon Nike gris con bolsillos", "M", 16050 , 10);
+const producto4 = new Producto ("Pantalon Adidas rojo sin bolsillos", "L", 18050, 4);
+const producto5 = new Producto ("Medias Nike blancas cortas", "M", 2050, 7);
+const producto6 = new Producto ("Musculosa Under Armour amarilla", "S", 9050, 6);
+const producto7 = new Producto ("Buzo Ascis verde poliester", "L", 12000, 8);
+
+producto1.vender();
+console.log(producto1);
 
 
 
